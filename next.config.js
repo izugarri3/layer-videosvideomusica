1de484a75853b67b3688f2803d63b268e481bbb5
Izugarri <57817632+Izugarri@users.noreply.github.com>
2021-08-17 11:02:35 +0200

    package

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
-module.exports = {
-  reactStrictMode: true,
-}
+const { withLayer0, withServiceWorker } = require('@layer0/next/config')
+
+  module.exports = withLayer0(
+    withServiceWorker({
+      layer0SourceMaps: true,
+      // ...
+      // additional Next.js config options here
+      // ...
+    })
+  )
