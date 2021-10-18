912c0299ac425f803b87c0f60d3c3884a277914e
Izugarri <57817632+Izugarri@users.noreply.github.com>
2021-10-18 15:52:18 +0200

    arranque con B4a

--- layer0.config.js
+++ layer0.config.js
@@ -1,8 +1,3 @@
 // This file was automatically added by layer0 deploy.
 // You should commit this file to source control.
-
-// layer0.config.js
-const defaultHostname = 'search.videomusica.es'
-module.exports = {
-  connector: '@layer0/next',
-}
+module.exports = {}
--- routes.js
+++ routes.js
@@ -1,10 +1,38 @@
-// This file was automatically added by layer0 deploy.
+// This file was added by layer0 init.
 // You should commit this file to source control.
+
 const { Router } = require('@layer0/core/router')
-const { nextRoutes } = require('@layer0/next')
+
+const ONE_MIN = 5 * 5
+const ONE_HOUR = 60 * 60
+const ONE_DAY = 24 * ONE_HOUR
+const ONE_YEAR = 365 * ONE_DAY
+
+const edgeOnly = {
+
+  edge: { maxAgeSeconds: 3500 },
+}
+
+const edgeAndBrowser = {
+
+  edge: { maxAgeSeconds: 3502 },
+}
 
 module.exports = new Router()
-  .match('/service-worker.js', ({ serviceWorker }) => {
-    return serviceWorker('.next/static/service-worker.js')
+  .prerender([{ path: '/' }])
+
+  // match client-side routes that aren't a static asset
+  // and serve the app shell. client-side router will
+  // handle the route once it is rendered
+  .match('/:path*/:file([^\\.]+|)', ({ appShell, cache }) => {
+    cache(edgeOnly)
+    appShell('public/index.html')
   })
-  .use(nextRoutes) // automatically adds routes for all files under /pages
+  
+  // match other assets such as favicon, manifest.json, etc
+  .match('/:path*', ({ serveStatic, cache }) => {
+  
+   serveStatic('public/:path*')
+  })
+  // send any unmatched request to serve the static index.html
+  .fallback(({ serveStatic }) => serveStatic('public/index.html'))
