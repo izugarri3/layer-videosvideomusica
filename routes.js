a67301198787ab69a9911b2191dcd5ceed8318d1
Izugarri <57817632+Izugarri@users.noreply.github.com>
2023-01-15 11:20:07 +0100

    rout

--- routes.js
+++ routes.js
@@ -1,29 +1,32 @@
-// This file was automatically added by edgio deploy.
-// You should commit this file to source control.
-import { Router } from '@edgio/core'
-import { reactCRARoutes } from '@edgio/react-cra'
-import { isProductionBuild } from '@edgio/core/environment'
-import { API_CACHE_HANDLER, EDGE_CACHE_HANDLER, IMAGE_CACHE_HANDLER } from './cache'
+// routes.js
 
-const router = new Router()
+const { Router } = require('@edgio/core/router')
 
-// Prevent search engines from indexing permalink URLs
-router.noIndexPermalink()
+const ONE_YEAR = 365 * 24 * 60 * 60
 
-// API (Any backend) caching
-router.match('/l0-api/:path*', API_CACHE_HANDLER)
-
-// Image caching
-router.match('/l0-opt', IMAGE_CACHE_HANDLER)
-
-if (isProductionBuild()) {
-  router.match('/', EDGE_CACHE_HANDLER)
-  router.match('/about', EDGE_CACHE_HANDLER)
-  router.match('/commerce', EDGE_CACHE_HANDLER)
-  router.match('/product/:name', EDGE_CACHE_HANDLER)
-  router.match('/commerce/:name', EDGE_CACHE_HANDLER)
+const edgeOnly = {
+  browser: false,
+  edge: { maxAgeSeconds: ONE_YEAR },
 }
 
-router.use(reactCRARoutes)
+const edgeAndBrowser = {
+  browser: { maxAgeSeconds: ONE_YEAR },
+  edge: { maxAgeSeconds: ONE_YEAR },
+}
 
-export default router
+const handler = ({ cache, serveStatic }, cacheConfig, path) => {
+  cache(cacheConfig)
+  serveStatic(path)
+}
+
+module.exports = new Router()
+
+  // Assets (Hashed and Cached on Edge and in the Browser)
+  .get('/css/:path*', res => handler(res, edgeAndBrowser, 'src/css/:path*')) 
+  .get('/js/:path*', res => handler(res, edgeAndBrowser, 'src/js/:path*')) 
+  
+  // Path(s) that do not have a "." as well as "/" to serve the fallback page
+  .get('/:path*/:file([^\\.]+|)', res => handler(res, edgeOnly, 'src/index.html')) 
+  
+  // All other paths to be served from the src directory
+  .get('/:path*', res => handler(res, edgeOnly, 'src/:path*'))
