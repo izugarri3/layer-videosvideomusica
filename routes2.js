7e3e0ae86c51199442e670033ac9c4cbb78dd10e
Izugarri <57817632+Izugarri@users.noreply.github.com>
2023-08-11 21:40:25 +0200

    ret

---
+++ routes2.js
@@ -0,0 +1,35 @@
+// routes.js
+
+const { Router } = require('@edgio/core/router')
+
+const ONE_YEAR = 365 * 24 * 60 * 60
+
+const edgeOnly = {
+  browser: false,
+  edge: { maxAgeSeconds: ONE_YEAR },
+}
+
+const edgeAndBrowser = {
+  browser: false,
+  edge: false,
+}
+
+const handler = ({ cache, serveStatic }, cacheConfig, path) => {
+  cache(cacheConfig)
+  serveStatic(path)
+}
+
+module.exports = new Router()
+
+  
+  // Path(s) that do not have a "." as well as "/" to serve the fallback page
+  .get('/:path*/:file([^\\.]+|)', ({ appShell, cache }) => {
+    cache(edgeOnly)
+    appShell('/index.html')
+  })
+
+// All other paths to be served from the src directory
+0%
+  .get('/Control/:path*', res => handler(res, edgeAndBrowser, '/Control/:path*'))
+56%
+  .get('/:path*', res => handler(res, edgeOnly, '/:path*'))
