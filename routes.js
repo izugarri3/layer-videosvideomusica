3880214356f83087fe20df800dfcf182e0967068
Izugarri <57817632+Izugarri@users.noreply.github.com>
2023-08-12 07:57:20 +0200

    Update routes.js

--- routes.js
+++ routes.js
@@ -1,5 +1,33 @@
-// This file was automatically added by edgio deploy.
-// You should commit this file to source control.
-import { Router } from '@edgio/core'
+// routes.js
 
-export default new Router().fallback(({ renderWithApp }) => renderWithApp())
+const { Router } = require('@edgio/core/router')
+
+const ONE_YEAR = 365 * 24 * 60 * 60
+
+const edgeOnly = {
+  browser: false,
+  edge: { maxAgeSeconds: ONE_YEAR },
+}
+
+const edgeAndBrowser = {
+  browser: false,
+  edge: false,
+}
+
+const handler = ({ cache, serveStatic }, cacheConfig, path) => {
+  cache(cacheConfig)
+  serveStatic(path)
+}
+
+module.exports = new Router()
+
+  
+  // Path(s) that do not have a "." as well as "/" to serve the fallback page
+  .get('/:path*/:file([^\\.]+|)', ({ appShell, cache }) => {
+    cache(edgeOnly)
+    appShell('public/index.html')
+  }) 
+  
+  // All other paths to be served from the src directory
+  .get('/Control/:path*', res => handler(res, edgeAndBrowser, 'public/Control/:path*'))
+  .get('/:path*', res => handler(res, edgeOnly, 'public/:path*'))
